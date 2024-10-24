17e0b0221c685bbaa1d8c87baa7ba9d6c6a5daca
Taniya Bansal <taniya1agarwal@gmail.com>
2024-10-24 20:09:04 +0530

    Add heroSection, gallary, textAnimation, roomAvailable and gallary page

--- src/app/page.js
+++ src/app/page.js
@@ -6,41 +6,42 @@ import AnimatedCursor from "react-animated-cursor";
 import img1 from '../components/arrow.png'
 import SparkleEffect from "@/components/Customcourser";
 // import ContactSection from "@/app/contact/page";
+import HeroSection from "@/components/HeroSection";
+import { LayoutGrid } from "@/components/LayoutGrid";
+import TextAnimation from "@/components/TextAnimation";
+import Rooms from "@/components/Rooms";
 
 export default function Home() {
   return (
     <>
-      <div >
-        {/* <SparkleEffect /> */}
-        <AnimatedCursor
-          innerSize={8}
-          outerSize={25}
-          color="255, 165, 0"
-          outerAlpha={0.2}
-          innerScale={0.7}
-          outerScale={2}
-          clickables={[
-            'a',
-            'input[type="text"]',
-            'input[type="email"]',
-            'input[type="number"]',
-            'input[type="submit"]',
-            'input[type="image"]',
-            'label[for]',
-            'select',
-            'textarea',
-            'button',
-            '.link',
-          ]}
-        />
-        
-        {/* Adjust Navbar position and z-index */}
-        <div className="fixed top-0 left-0 w-full z-50">
-        </div>
-
-        <div className=""> {/* Add padding to avoid overlap with fixed Navbar */}
-          {/* <ContactSection /> */}
-        </div>
+     <div className="bg-[#FFFFFF]">
+      <SparkleEffect/>
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={25}
+        // RGB values for white color
+        color="255, 165, 0"
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={2}
+        clickables={[
+          'a',
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          'label[for]',
+          'select',
+          'textarea',
+          'button',
+          '.link',
+        ]}
+      />
+      <HeroSection />
+      <LayoutGrid />
+      <TextAnimation />
+      <Rooms />
       </div>
     </>
   );
